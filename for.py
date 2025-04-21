50ba8beefdc623747882362f9782118d5a4f6397
Lilyennd <victoriariffo1@gmail.com>
2025-04-21 11:18:38 -0400

    Ultima actividad

--- for.py
+++ for.py
@@ -51,4 +51,82 @@
 #         print("usted reprobo")
 
 
+# # pida al usuario un numero y sume todos los digitos anteriores
+# suma=0
+# rango=int(input("ingrese un numero"))
+# for i in range(rango):
+#     suma+=i
+#     print("la suma de los numeros anteriores es", suma)
+
+
+# pida al usuario la cantidad de numeros  y verifique que cada uno sea par o impar
+
+# Cant=int(input("cuantos numeros desea verificar?"))
+# for i in range(Cant):
+#     print("insgrese un numero")
+#     num=int(input())
+#     if num%2==0:
+#         print("su numero es par")
+#     else:
+#         print ("su numero es impar")
+
+
+# Cant=int(input("hasta que numero desea verificar?"))
+# for i in range(Cant):
+#     i+=1
+#     if i%2==0:
+#         print("su numero es par")
+#     else:
+#         print ("su numero es impar")
+
+# Algoritmo votos
+# 	Escribir "ingrese un numero de votantes"
+# 	Leer num
+	
+# 	kaiser=0
+# 	nose=0
+	
+# 	Para i<-1 Hasta num Con Paso 1 Hacer
+# 		Escribir "ingrese su voto: Kaiser 1, Nose 2"
+# 		Leer voto
+# 		Si voto==1 Entonces
+# 			kaiser=kaiser+1
+# 		SiNo
+# 			Si voto==2 Entonces
+# 				nose=nose+1
+# 			SiNo
+# 				Escribir "Error, selecione un voto válido"
+# 			Fin Si
+# 		Fin Si
+# 	Fin Para
+# 	Escribir "los votos de Kaiser son ",kaiser
+# 	Escribir "los votos de Nose son ",nose
+# 	Si kaiser>nose Entonces
+# 		Escribir "Gano Kaiser"
+# 	SiNo
+# 		Escribir "Gano Nose"
+# 	Fin Si
+# FinAlgoritmo
+print("Primera candidata Dobby")
+Dobby=0
+print("Segundo candidato Morzat")
+Morzat=0
+Cant=int(input("ingrese cantidad de votantes"))
+for i in range(Cant):
+    voto=int(input("vote 1 por dobby, o 2 por morzat"))
+    if voto==1:
+        Dobby+=1
+    elif voto==2:
+        Morzat+=1
+    else:
+        print("ERROR")
+        print("los votos de dobby son", Dobby)
+        print("los votos de morzat son", Morzat)
+        if Dobby > Morzat:
+            print("Gano Dobby")
+        elif Morzat > Dobby:
+            print("Gano Morzat")
+        else:
+            print("Es un empate")
+
 
