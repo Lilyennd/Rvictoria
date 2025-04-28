e236356946e9464f8fd572a03768c8aac5504aa0
Lilyennd <victoriariffo1@gmail.com>
2025-04-28 11:12:06 -0400

    trabajo clases

--- Ejercicios.py
+++ Ejercicios.py
@@ -25,3 +25,59 @@
 
 
 
+#designe dos jugadores solicitando sus nombres
+#la pelea es por turno
+# cada jugador tiene 50 hp 
+#debe mostrar barra de energia
+#cada jugador tiene entre 3 y 15 de ataque
+#crit de 20%
+
+# import random
+# print("ingrese el nombre delos jugadores")
+# j1=input("jugador 1")
+# j2=input("jugador 2")
+# hp1=50
+# hp2=50
+# turno= random.randint(1,2)
+# while hp1>0 and hp2>0:
+#     if turno %2==0:
+#         print("turno de", j1)
+#         ataque=random.randint(3,15)
+#         crit=random.randint(1,5)
+#         if crit==1:
+#             ataque=ataque*2
+#             print(" ataque critico")
+#         hp2-=ataque
+#         print("/"*hp1)
+#         print(f"la vida de {j1} es {hp1}")
+#         turno+=1
+#     else:
+#         print("turno de", j2)
+#         ataque=random.randint(3,15)
+#         crit=random.randint(1,5)
+#         if crit==1:
+#             ataque=ataque*2
+#             print(" ataque critico")
+#         hp1-=ataque
+#         print("/"*hp2)
+#         print(f"la vida de {j2} es {hp2}")
+#         turno+=1
+
+# if hp1<=0:
+#     print(f"gano {j2}")
+# elif hp2<=0:
+#     print(f"gano {j1}")
+
+
+#CREAR UN CAJERO AUTOMATICO
+# TENER EN CUENTA CAJAS DE BILLETES DE 5MIL,10MIL Y 20MIL
+# HAY CAJAS DE 30 BILLETES
+# EL USUARIO INGRESA LA CANTIDAD QUE QUIERE RETIRAR Y VERIFICAR SI HAY BILLETES FUFICIENTES, MOSTRAR SALDO DISPONIBLE
+# 3 USUARIOS CON SU CLAVE CORRESPONDIENTE
+#CADA USUARIO TIENE UNA CLAVE
+
+U1= 1123
+U2= 1223
+U3= 1233
+saldo=0
+clave=int(input("ingrese su clave"))
--- while.py
+++ while.py
@@ -9,15 +9,61 @@
 
 # print("su clave es correcta")
 
-clave=3311
-intentos=3
-password=int(input("ingrese su password"))
-while clave!=password and intentos<=3:
-    intentos-=1
-    print("error, clave invalida")
-    password=int(input("ingrese su password"))
+# clave=3311
+# intentos=3
+# password=int(input("ingrese su password"))
+# while clave!=password and intentos<=3:
+#     intentos-=1
+#     print("error, clave invalida")
+#     password=int(input("ingrese su password"))
 
-if intentos>=1:
-    print("se han acabado sus intentos")
-else:
-    print("su clave es correcta")
+# if intentos>=1:
+#     print("se han acabado sus intentos")
+# else:
+#     print("su clave es correcta")
+
+
+# suma=0
+# while True:
+#     numero=int(input("ingrese un numero"))
+#     if numero==0:
+#         break
+#     suma+=numero
+# print("la suma de los numeros es", suma)
+
+#pida al usuario el limite superior e inferior de un rango y genere un numero al azar dentro de ese numero 
+#el segundo numero no puede ser menor
+
+# #aleatorios
+# print("ingrese dos numeros")
+# l1=int(input("ingrese el limite inferior"))
+# l2=int(input("ingrese el limite superior"))
+# while l2<l1:
+#     print("el limite superior no puede ser menor que el inferior")
+#     l2=int(input("ingrese el limite superior"))
+    
+# import random
+# numero=random.randint(l1,l2)
+# print("el numero es", numero)
+
+
+#limite entre 1,50
+#ir dando pitas sobre el numero
+# mayor o menor
+
+
+# import random
+# resp=random.randint(1,50)
+# print(resp)
+# intentos=0
+# answ=int(input("ingrese un numero entre 1 y 50")) 
+# while answ!=resp:
+#     intentos+=1
+#     if intentos==5:
+#         print("se han acabado sus intentos")
+#         break
+#     if answ>resp:
+#         print("el numero que bsucas es menor")
+#     else:
+#         print("el numero que buscas es mayor")
+#     answ=int(input("ingrese un numero entre 1 y 50"))
