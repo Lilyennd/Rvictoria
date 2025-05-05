3241aaf50d64bd5358179c49628b04d09a72ffa7
Lilyennd <victoriariffo1@gmail.com>
2025-05-05 10:57:47 -0400

    clase antes de la prueba

--- Ejercicios.py
+++ Ejercicios.py
@@ -217,63 +217,212 @@
 #APLICAR EL DESCUENTO SEGUN EL TOTAL
 
 
-total=0
-print("elija 3 productos por categoria")
-print('''
-      1.reposteria
-      2. frutas
-      3.lacteos
-      ''')
-cat=int(input())
+# total=0
+# print("elija 3 productos por categoria")
+# print('''
+#       1.reposteria
+#       2. frutas
+#       3.lacteos
+#       ''')
+# cat=int(input())
 
-if cat==1:
-    print('''
-        1. mezclas postres 45
-        2. fondant 60
-        3. chispas chocolate 25
-      ''')
-    op=int(input())
-    if op==1:
-        total+=45+200
-    elif op==2:
-        total+=60+200
-    elif op==3:
-        total+=25+200
-elif cat==2:
-    print('''
-        1. manzana 15
-        2. pera 23
-        3. uva 19
-      ''')
-    op=int(input())
-    if op==1:
-        total+=15+400
-    elif op==2:
-        total+=23+400
-    elif op==3:
-        total+=19+400
-elif cat==3:
-    print('''
-        1. leche 20
-        2. yogurt 12
-        3. helado vainilla 30
-      ''')
-    op=int(input())
-    if op==1:
-        total+=20+600
-    elif op==2:
-        total+=12+600
-    elif op==3:
-        total+=30+600
-else:
-    print("elija una opcion valida")
+# if cat==1:
+#     print('''
+#         1. mezclas postres 45
+#         2. fondant 60
+#         3. chispas chocolate 25
+#       ''')
+#     op=int(input())
+#     if op==1:
+#         total+=45+200
+#     elif op==2:
+#         total+=60+200
+#     elif op==3:
+#         total+=25+200
+# elif cat==2:
+#     print('''
+#         1. manzana 15
+#         2. pera 23
+#         3. uva 19
+#       ''')
+#     op=int(input())
+#     if op==1:
+#         total+=15+400
+#     elif op==2:
+#         total+=23+400
+#     elif op==3:
+#         total+=19+400
+# elif cat==3:
+#     print('''
+#         1. leche 20
+#         2. yogurt 12
+#         3. helado vainilla 30
+#       ''')
+#     op=int(input())
+#     if op==1:
+#         total+=20+600
+#     elif op==2:
+#         total+=12+600
+#     elif op==3:
+#         total+=30+600
+# else:
+#     print("elija una opcion valida")
     
 
-if total>=1000:
-    total=total*0.97
-elif total>=1001 and total<=5000:
-    total=total*0.95
-elif total>=5001:
-    total=total*0.94
+# if total>=1000:
+#     total=total*0.97
+# elif total>=1001 and total<=5000:
+#     total=total*0.95
+# elif total>=5001:
+#     total=total*0.94
 
-print("su total es", total)
+# print("su total es", total)
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#clase 05/05
+
+
+#calcular puntaje de credito
+# sedebecalcular que tanto credito tiene una persona
+#en cierta entidad financiera debe considerar
+# ingresos mensuales, nivel educacional y nacionalidad
+# cantidad de ingresos
+#500mil a 1palo= 300.000
+#1palo a 1.5palos= 650000
+#1.5palos o mas = 1.000.000
+# nivel educacional
+#basico: x1 mediao: x1,3 superior: x1,5
+# nacionalidad
+# chileno: +300mil , extranjero: +0
+
+
+
+# credito=0
+# print("elija su rango de ingresos")
+# print('''
+#       1.500.000 a 1.000.000
+#       2. 1.000.001 a 1.500.000
+#       3.1.500.000 o superior
+#       ''')
+# op=int(input())
+# if op==1:
+#     credito+=300000
+# elif op==2:
+#      credito+=650000
+# elif op==3:
+#      credito+=1000000
+# else:
+#     print("elija una opcion valida")
+
+# print("elija su nivel educacional")
+# print('''
+#       1.Basica
+#       2.Media
+#       3.Superior
+#       ''')
+# op=int(input())
+# if op==1:
+#      credito*=1
+# elif op==2:
+#      credito*=1.3
+# elif op==3:
+#      credito*=1.5
+# else:
+#     print("elija una opcion valida")
+
+# print("elija su nacionalidad")
+# print('''
+#       1.Chilena
+#       2.Otra
+#       ''')
+# op=int(input())
+# if op==1:
+#      credito+=300000
+# elif op==2:
+#      credito+=0
+# print(f"su puntaje de credito es {credito}")
+
+
+
+#pedir dia y mes de nacimiento y mostrar el signo zodiacal
+#Aries: 21 de marzo al 19 de abril                               21/03 - 19/04
+# Tauro: 20 de abril y al 20 de mayo                             20/04 - 20/05
+# Géminis: 21 de mayo al 20 de junio
+# Cáncer: 21 de junio al 22 de julio
+# Leo: 23 de julio al 22 de agosto
+# Virgo: 23 de agosto al 22 de septiembre
+# Libra: 23 de septiembre al 22 de octubre
+# Escorpio: 23 de octubre al 21 de noviembre
+# Sagitario: 22 de noviembre al 21 de diciembre
+# Capricornio: 22 de diciembre al 19 de enero
+# Acuario: 20 de enero al 18 de febrero
+# Piscis: 19 de febrero al 20 de marzo
+
+
+
+
+
+
+
+
+#pida al usuario 2 numeros verificando que el 2do sea mayor
+#genere un numero aleatorio entre esos dos numeros 
+# imprima la cantidad de veces el simbolor ▄
+# import random
+# print("ingrese dos digitos, el segundo debera ser mayor al primero")
+# N1=(int(input()))
+# N2=(int(input()))
+
+# while N1>N2:
+#     print("su segundo numero es menor al primero")
+#     N2=(int(input()))
+
+# N=random.randint(N1,N2)
+# # for i in range (N):
+# #     print ("▄")
+# print("▄"*N)
+
+
+
+#crear un programa que pida la cantidad de ramos 
+#pida el promedio por materia
+#basado en su promedio final aplicar puntaje de f¿beneficio
+#4,5 y 5: 300 - 5.0 y 6.0:500 - 6.1 y 7.0 : 800
+#agregar puntaje segun carrera
+#tecnico mas +60, ing +40, diplomado +20
+beneficio=0
+prom=0
+c=(int(input("ingrese cantidad de ramos")))
+for i in range (c):
+    materia=(float(input(f"ingrese la nota de la materia {i+1}")))
+    prom+=materia/c
+print("su promedio es",prom)
+
+if prom>4.5 and prom<5.0:
+    print("su beneficio es de ", 300)
+    beneficio+=300
+elif prom>5.1 and prom<6.0:
+    print("su beneficio es de ", 500)
+    beneficio+=500
+elif prom>6.1 and prom<7.0:
+    print("su beneficio es de ", 800)
+    beneficio+=800
+else:
+    print("no posee beneficios")
+
+print("defina su carrera")
+print('''
+      1.tecnico
+      2.ingenieria
+      3.diplomado
+      ''')
+op=int(input())
+if op==1:
+    beneficio+=60
+if op==2:
+    beneficio+=40
+if op==3:
+    beneficio+=20
+
+print(f"su beneficio final es de {beneficio}")
